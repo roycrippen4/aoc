da5378e55ec9bc9acfc95c699bd8f4a37dd1b0a5
roy.crippen4 <roy.crippen4@archarithms.com>
2025-01-06 18:19:23 -0500

    feat(2024): day 08 both parts done

--- aoc2024/src/day08/part1.rs
+++ aoc2024/src/day08/part1.rs
@@ -1,72 +1,91 @@
-#![allow(unused)]
-use crate::util::{create_pad, StringMethods};
+use std::collections::{HashMap, HashSet};
 
-type Mapping = (usize, Vec<usize>);
+type Row = Vec<char>;
+type Mapping = HashMap<char, Vec<(usize, usize)>>;
+type Point = (usize, usize);
 
-fn into_padded_string(str: &&str) -> String {
-    let mut s = str.to_string();
-    s.pad(4, '.');
-    s
-}
+fn rotate_point(p: Point, pivot: Point, width: usize, height: usize) -> Option<Point> {
+    let (x, y) = p;
+    let (px, py) = pivot;
+    let x2 = px.checked_mul(2).and_then(|two_px| two_px.checked_sub(x))?;
+    let y2 = py.checked_mul(2).and_then(|two_py| two_py.checked_sub(y))?;
 
-#[derive(Debug)]
-struct Grid {
-    data: Vec<Vec<char>>,
-}
-
-impl Grid {
-    pub fn new(data: Vec<&str>) -> Self {
-        let mut data: Vec<String> = data.iter().map(into_padded_string).collect();
-        let row_len = data[0].len();
-        let pad = create_pad(row_len, '.');
-        (0..4).for_each(|_| data.insert(0, pad.clone()));
-        let col_len = data.len();
-        (0..4).for_each(|_| data.insert(col_len, pad.clone()));
-        Self {
-            data: data.iter().map(|r| r.to_char_vec()).collect(),
-        }
+    if x2 < width && y2 < height {
+        Some((x2, y2))
+    } else {
+        None
     }
 }
 
-#[allow(unused)]
+fn get_antennas(grid: &[Row]) -> Mapping {
+    let mut map: Mapping = HashMap::new();
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            let c = grid[y][x];
+            if c.is_alphanumeric() {
+                map.entry(c)
+                    .and_modify(|vec| vec.push((x, y)))
+                    .or_insert(vec![(x, y)]);
+            }
+        }
+    }
+
+    map
+}
+
 fn example(data: &str) -> usize {
-    0
+    let grid: Vec<_> = data.lines().map(|r| r.chars().collect()).collect();
+    let antennas = get_antennas(&grid);
+    let width = grid[0].len();
+    let height = grid.len();
+    let mut nodes: HashSet<Point> = HashSet::new();
+
+    for positions in antennas.values() {
+        for i in 0..positions.len() - 1 {
+            for j in i + 1..positions.len() {
+                if let Some(p) = rotate_point(positions[i], positions[j], width, height) {
+                    nodes.insert(p);
+                };
+                if let Some(p) = rotate_point(positions[j], positions[i], width, height) {
+                    nodes.insert(p);
+                }
+            }
+        }
+    }
+    nodes.len()
 }
 
 pub fn solve() -> usize {
-    // include_str!("data/data.txt");
-    0
+    example(include_str!("data/data.txt"))
 }
 
 #[cfg(test)]
 mod test {
+
     use crate::util::{validate, Day, Kind};
 
     use super::{example, solve};
 
     #[test]
     fn test_solve() {
-        dbg!(solve());
-        // validate(solve, 303766880536, Day::Day07, Kind::Part1);
+        validate(solve, 244, Day::Day08, Kind::Part1);
     }
 
     #[test]
     fn test_example() {
         let data = include_str!("data/example.txt");
-        let _ = example(data);
-        // assert_eq!(3749, example());
+        assert_eq!(14, example(data));
     }
 
     #[test]
     fn test_simple_example() {
         let data = include_str!("./data/example-simple.txt");
-        let _ = example(data);
-        // assert_eq!(3749, example());
+        assert_eq!(2, example(data))
     }
     #[test]
     fn test_simple_example2() {
         let data = include_str!("./data/example-simple2.txt");
-        let _ = example(data);
-        // assert_eq!(3749, example());
+        assert_eq!(4, example(data));
     }
 }
--- aoc2024/src/day08/part2.rs
+++ aoc2024/src/day08/part2.rs
@@ -1,72 +1,103 @@
-#![allow(unused)]
-use crate::util::{create_pad, StringMethods};
+use std::collections::{HashMap, HashSet};
 
-type Mapping = (usize, Vec<usize>);
+type Row = Vec<char>;
+type Mapping = HashMap<char, Vec<(usize, usize)>>;
+type Point = (usize, usize);
 
-fn into_padded_string(str: &&str) -> String {
-    let mut s = str.to_string();
-    s.pad(4, '.');
-    s
-}
+fn rotate_point(p: Point, pivot: Point, width: usize, height: usize) -> Option<Point> {
+    let (x, y) = p;
+    let (px, py) = pivot;
+    let x2 = px.checked_mul(2).and_then(|two_px| two_px.checked_sub(x))?;
+    let y2 = py.checked_mul(2).and_then(|two_py| two_py.checked_sub(y))?;
 
-#[derive(Debug)]
-struct Grid {
-    data: Vec<Vec<char>>,
-}
-
-impl Grid {
-    pub fn new(data: Vec<&str>) -> Self {
-        let mut data: Vec<String> = data.iter().map(into_padded_string).collect();
-        let row_len = data[0].len();
-        let pad = create_pad(row_len, '.');
-        (0..4).for_each(|_| data.insert(0, pad.clone()));
-        let col_len = data.len();
-        (0..4).for_each(|_| data.insert(col_len, pad.clone()));
-        Self {
-            data: data.iter().map(|r| r.to_char_vec()).collect(),
-        }
+    if x2 < width && y2 < height {
+        Some((x2, y2))
+    } else {
+        None
     }
 }
 
-#[allow(unused)]
-fn example(data: &str) -> usize {
-    0
+fn get_antennas(grid: &[Row]) -> Mapping {
+    let mut map: Mapping = HashMap::new();
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            let c = grid[y][x];
+            if c.is_alphanumeric() {
+                map.entry(c)
+                    .and_modify(|vec| vec.push((x, y)))
+                    .or_insert(vec![(x, y)]);
+            }
+        }
+    }
+
+    map
+}
+
+fn place_nodes(p1: Point, p2: Point, width: usize, height: usize, nodes: &mut HashSet<Point>) {
+    nodes.insert(p1);
+    nodes.insert(p2);
+
+    let mut a = p1;
+    let mut b = p2;
+    while let Some(next) = rotate_point(a, b, width, height) {
+        nodes.insert(next);
+        a = b;
+        b = next;
+    }
+
+    let mut a = p2;
+    let mut b = p1;
+    while let Some(next) = rotate_point(a, b, width, height) {
+        nodes.insert(next);
+        a = b;
+        b = next;
+    }
+}
+
+fn evaluate(data: &str) -> usize {
+    let grid: Vec<_> = data.lines().map(|r| r.chars().collect()).collect();
+    let antennas = get_antennas(&grid);
+    let width = grid[0].len();
+    let height = grid.len();
+    let mut nodes: HashSet<Point> = HashSet::new();
+
+    for positions in antennas.values() {
+        for i in 0..positions.len() - 1 {
+            for j in i + 1..positions.len() {
+                let p1 = positions[i];
+                let p2 = positions[j];
+                place_nodes(p1, p2, width, height, &mut nodes);
+            }
+        }
+    }
+    nodes.len()
 }
 
 pub fn solve() -> usize {
-    // include_str!("data/data.txt");
-    0
+    evaluate(include_str!("data/data.txt"))
 }
 
 #[cfg(test)]
 mod test {
+
     use crate::util::{validate, Day, Kind};
 
-    use super::{example, solve};
+    use super::{evaluate, solve};
 
     #[test]
     fn test_solve() {
-        dbg!(solve());
-        // validate(solve, 303766880536, Day::Day07, Kind::Part1);
+        validate(solve, 912, Day::Day08, Kind::Part1);
     }
 
     #[test]
     fn test_example() {
         let data = include_str!("data/example.txt");
-        let _ = example(data);
-        // assert_eq!(3749, example());
+        assert_eq!(34, evaluate(data));
     }
 
     #[test]
-    fn test_simple_example() {
-        let data = include_str!("./data/example-simple.txt");
-        let _ = example(data);
-        // assert_eq!(3749, example());
-    }
-    #[test]
-    fn test_simple_example2() {
-        let data = include_str!("./data/example-simple2.txt");
-        let _ = example(data);
-        // assert_eq!(3749, example());
+    fn test_example_part2() {
+        let data = include_str!("data/example-part2.txt");
+        assert_eq!(9, evaluate(data))
     }
 }
