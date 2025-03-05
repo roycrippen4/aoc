b969e73fb9c2631f6b2b768b53d4fcb52009dba0
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-04 23:36:46 -0500

    fix(ocaml2024): wrong data filepath

--- aoc2024/aoc_ocaml/lib/day03.ml
+++ aoc2024/aoc_ocaml/lib/day03.ml
@@ -1,6 +1,6 @@
 open Util
 
-let input = read_to_string "/home/roy/dev/aoc/aoc2024/data/day02/data.txt"
+let input = read_to_string "/home/roy/dev/aoc/aoc2024/data/day03/data.txt"
 let re_part1 = Re.Perl.compile_pat "mul\\(\\d{1,3},\\d{1,3}\\)"
 let trim mat = slice mat 4 (String.length mat - 1)
 let split_on_comma s = Re.split (Re.compile (Re.char ',')) s
@@ -13,6 +13,11 @@ let eval_mul acc lst =
 let solve1 () =
   List.map trim (Re.matches re_part1 input)
   |> List.map split_on_comma |> List.fold_left eval_mul 0
+
+let solve2 () = 42
+let part1 () = validate solve1 173731097 "03" One
+let part2 () = validate solve2 42 "03" Two
+let solution : solution = { part1; part2 }
 
 (* Just let me space this out ocamlformat *)
 let%test _ =
--- aoc2024/aoc_ocaml/lib/lib.ml
+++ aoc2024/aoc_ocaml/lib/lib.ml
@@ -1,2 +1,3 @@
 let day01 = Day01.solution
 let day02 = Day02.solution
+let day03 = Day03.solution
