317f268f4098ae95567ba6e8891e0bdbe38c3c1f
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-06 22:48:59 -0500

    perf(ocaml2024): Major speedup in day 01 part 2

--- aoc2024/aoc_ocaml/lib/day01.ml
+++ aoc2024/aoc_ocaml/lib/day01.ml
@@ -1,7 +1,6 @@
 open Util
 
 let lines = read_to_lines "/home/roy/dev/aoc/aoc2024/data/day01/data.txt"
-let example = read_to_lines "/home/roy/dev/aoc/aoc2024/data/day01/example.txt"
 
 let split_to_side (left, right) line =
   let split_line = split_to_string ' ' line |> Array.of_list in
@@ -16,28 +15,40 @@ let solve1 () =
   Array.init (Array.length left) (fun i -> abs (left.(i) - right.(i)))
   |> Array.fold_left ( + ) 0
 
-let parse_line (left, right) line =
-  match split_to_string ' ' line with
-  | [ l; r ] -> (left @ [ int_of_string l ], right @ [ int_of_string r ])
-  | _ -> assert false
+let into_frequency_map lst =
+  let map = Hashtbl.create 500 in
+  let f x =
+    let count = try Hashtbl.find map x with Not_found -> 0 in
+    Hashtbl.replace map x (count + 1)
+  in
+  List.iter f lst;
+  map
+
+let parse lines =
+  let rec inner_parse acc_left acc_right = function
+    | [] -> (List.rev acc_left, List.rev acc_right)
+    | line :: rest -> (
+        match
+          line |> String.split_on_char ' '
+          |> List.filter (fun s -> s <> "")
+          |> List.map int_of_string
+        with
+        | [ l; r ] -> inner_parse (l :: acc_left) (r :: acc_right) rest
+        | _ -> assert false)
+  in
+  inner_parse [] [] lines
 
 let count_elems lst n = n * (List.filter (fun x -> x = n) lst |> List.length)
 
 let solve2 () =
-  let left, right = List.fold_left parse_line ([], []) lines in
-  List.fold_left (fun acc l -> acc + count_elems right l) 0 left
+  let left_map, right_map = parse lines |> map_tuple into_frequency_map in
+  Hashtbl.fold
+    (fun n count acc ->
+      match Hashtbl.find_opt right_map n with
+      | Some count_right -> acc + (n * count * count_right)
+      | None -> acc)
+    left_map 0
 
 let part1 () = validate solve1 1506483 "01" One
 let part2 () = validate solve2 23126924 "01" Two
 let solution : solution = { part1; part2 }
-
-let%test _ =
-  let lefts, rights = parse_line ([], []) "60236   87497" in
-  let e_lefts, e_rights = ([ 60236 ], [ 87497 ]) in
-  e_lefts = lefts && rights = e_rights
-
-let%test _ =
-  let lefts, rights = parse_line ([], []) "60236   87497" in
-  let lefts, rights = parse_line (lefts, rights) "27507   18604" in
-  let e_lefts, e_rights = ([ 60236; 27507 ], [ 87497; 18604 ]) in
-  e_lefts = lefts && rights = e_rights
