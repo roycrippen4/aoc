0b109613e2b5ea7216946dbb9a796c6e8d3670e5
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-07 22:45:28 -0500

    chore: formatting

--- aoc2024/aoc_ocaml/lib/util/general.ml
+++ aoc2024/aoc_ocaml/lib/util/general.ml
@@ -1,15 +1,16 @@
-(** Returns [string array] from the file contents located at [filepath] separated by newlines *)
+(** Returns [string list] from the file contents located at [filepath] separated
+    by newlines *)
 let read_to_lines filepath =
   In_channel.with_open_text filepath In_channel.input_lines
 
 let read_to_string filepath =
   In_channel.with_open_text filepath In_channel.input_all
 
-(** Split the string [str] by character [delim] and filter empty results out  *)
+(** Split the string [str] by character [delim] and filter empty results out *)
 let split_to_string delim str =
   String.split_on_char delim str |> List.filter (fun s -> s <> "")
 
-(** Split the string [str] by character [delim] and filter empty results out  *)
+(** Split the string [str] by character [delim] and filter empty results out *)
 let split_to_int delim str =
   String.split_on_char delim str
   |> List.filter (fun s -> s <> "")
@@ -18,8 +19,8 @@ let split_to_int delim str =
 (** applies function [f] to the tuple [(a, b)] as [(f a, f b)] *)
 let map_tuple f (a, b) = (f a, f b)
 
-(** Colors string `s` fg color with `r`, `g`, `b` values using ansci escape codes.
-   `r`, `g`, and `b` values range from 0 to 255; *)
+(** Colors string `s` fg color with `r`, `g`, `b` values using ansci escape
+    codes. `r`, `g`, and `b` values range from 0 to 255; *)
 let rgb str r g b = Printf.sprintf "\x1b[38;2;%d;%d;%dm%s\x1b[0m" r g b str
 
 let destructure_list_pair = function
