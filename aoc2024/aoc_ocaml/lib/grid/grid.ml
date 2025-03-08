ef98d6ac8a9f30d0f5ae1a846979646b32ff5840
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-07 22:44:49 -0500

    feat(ocaml2024): grid module

---
+++ aoc2024/aoc_ocaml/lib/grid/grid.ml
@@ -0,0 +1,137 @@
+type 'a t = 'a array array
+type position = int * int
+
+let height g = Array.length g
+let width g = Array.length g.(0)
+let size g = (height g, width g)
+
+let make h w v =
+  if h < 1 || w < 1 then invalid_arg "Grid.make";
+  Array.make_matrix h w v
+
+let init h w f =
+  if h < 1 || w < 1 then invalid_arg "Grid.init";
+  Array.init h (fun i -> Array.init w (fun j -> f (i, j)))
+
+let copy g = init (height g) (width g) (fun (i, j) -> g.(i).(j))
+let inside g (i, j) = 0 <= i && i < height g && 0 <= j && j < width g
+let get g (i, j) = g.(i).(j)
+let set g (i, j) v = g.(i).(j) <- v
+
+type direction = N | NW | W | SW | S | SE | E | NE
+
+let move d (i, j) =
+  match d with
+  | N -> (i - 1, j)
+  | NW -> (i - 1, j - 1)
+  | W -> (i, j - 1)
+  | SW -> (i + 1, j - 1)
+  | S -> (i + 1, j)
+  | SE -> (i + 1, j + 1)
+  | E -> (i, j + 1)
+  | NE -> (i - 1, j + 1)
+
+let north = move N
+let north_west = move NW
+let west = move W
+let south_west = move SW
+let south = move S
+let south_east = move SE
+let east = move E
+let north_east = move NE
+
+let rotate_left g =
+  let h = height g and w = width g in
+  init w h (fun (i, j) -> g.(j).(w - 1 - i))
+
+let rotate_right g =
+  let h = height g and w = width g in
+  init w h (fun (i, j) -> g.(h - 1 - j).(i))
+
+let map f g = init (height g) (width g) (fun p -> f p (get g p))
+
+let iter4 f g p =
+  let f p = if inside g p then f p (get g p) in
+  f (north p);
+  f (west p);
+  f (south p);
+  f (east p)
+
+let iter8 f g p =
+  let f p = if inside g p then f p (get g p) in
+  f (north p);
+  f (north_west p);
+  f (west p);
+  f (south_west p);
+  f (south p);
+  f (south_east p);
+  f (east p);
+  f (north_east p)
+
+let fold4 f g p acc =
+  let f p acc = if inside g p then f p (get g p) acc else acc in
+  acc |> f (north p) |> f (west p) |> f (south p) |> f (east p)
+
+let fold8 f g p acc =
+  let f p acc = if inside g p then f p (get g p) acc else acc in
+  acc
+  |> f (north p)
+  |> f (north_west p)
+  |> f (west p)
+  |> f (south_west p)
+  |> f (south p)
+  |> f (south_east p)
+  |> f (east p)
+  |> f (north_east p)
+
+let iter f g =
+  for i = 0 to height g - 1 do
+    for j = 0 to width g - 1 do
+      f (i, j) g.(i).(j)
+    done
+  done
+
+let fold f g acc =
+  let rec fold ((i, j) as p) acc =
+    if i = height g then acc
+    else if j = width g then fold (i + 1, 0) acc
+    else fold (i, j + 1) (f p g.(i).(j) acc)
+  in
+  fold (0, 0) acc
+
+let find f g =
+  let exception Found of position in
+  try
+    iter (fun p c -> if f p c then raise (Found p)) g;
+    raise Not_found
+  with Found p -> p
+
+let read c =
+  let rec scan rows =
+    match input_line c with
+    | s -> scan (s :: rows)
+    | exception End_of_file ->
+        let row s = Array.init (String.length s) (String.get s) in
+        let g = Array.map row (Array.of_list (List.rev rows)) in
+        if Array.length g = 0 then invalid_arg "Grid.read";
+        let w = Array.length g.(0) in
+        for i = 1 to height g - 1 do
+          if Array.length g.(i) <> w then invalid_arg "Grid.read"
+        done;
+        g
+  in
+  scan []
+
+let print ?(bol = fun _fmt _i -> ())
+    ?(eol = fun fmt _i -> Format.pp_print_newline fmt ())
+    ?(sep = fun _fmt _p -> ()) p fmt g =
+  for i = 0 to height g - 1 do
+    bol fmt i;
+    for j = 0 to width g - 1 do
+      p fmt (i, j) g.(i).(j);
+      if j < width g - 1 then sep fmt (i, j)
+    done;
+    eol fmt i
+  done
+
+let print_chars = print (fun fmt _ c -> Format.pp_print_char fmt c)
---
+++ aoc2024/aoc_ocaml/lib/grid/grid.mli
@@ -0,0 +1,174 @@
+(** {2 Grids aka two-dimensional arrays}
+
+    Following conventions in mathematics, grids are considered rows first. Thus
+    in the following, [height] refers to the first dimension and [width] to the
+    second dimension.
+
+    {v
+             0   1       j      width-1
+           +---+---+---+---+---+--+
+        0  |   |   |   |   |   |  |
+           +---+---+---+---+---+--+
+        1  |   |   |   |   |   |  |
+           +---+---+---+---+---+--+
+        i  |   |   |   | X |   |  |
+           +---+---+---+---+---+--+
+  height-1 |   |   |   |   |   |  |
+           +---+---+---+---+---+--+
+    v}
+
+    Following OCaml conventions, indices are 0-based.
+
+    For the width to be well-defined, some of the following functions assume a
+    positive number of rows (and, to be consistent, a positive number of
+    columns). *)
+
+type 'a t = 'a array array
+
+type position = int * int
+(** A position is an ordered pair [(i,j)], where [i] is the row and [j] is the
+    column. Rows and columns are 0-based. *)
+
+val height : 'a t -> int
+(** Returns the number of rows. *)
+
+val width : 'a t -> int
+(** Returns the number of columns. *)
+
+val size : 'a t -> int * int
+(** both height and width, in that order *)
+
+val make : int -> int -> 'a -> 'a t
+(** [make h w v] returns a new grid with height [h] and width [w], where all
+    values are equal to [v]. Raises [Invalid_argument] if [h<1] or [w<1].
+
+    For [h>=1] and [w>=1], this is equivalent to [Array.make_matrix h w v]. *)
+
+val init : int -> int -> (position -> 'a) -> 'a t
+(** [init h w f] returns a new grid with height [h] and width [w], where the
+    value at position [p] is [f p]. Raises [Invalid_argument] if [h<1] or [w<1].
+*)
+
+val copy : 'a t -> 'a t
+(** [copy g] returns a new grid that contains the same elements as [g] *)
+
+val get : 'a t -> position -> 'a
+(** [get g p] returns the value at position [p]. Raises [Invalid_argument] if
+    the position is out of bounds. *)
+
+val set : 'a t -> position -> 'a -> unit
+(** [set g p v] sets the value at position [p], with [v]. Raises
+    [Invalid_argument] if the position is out of bounds. *)
+
+val inside : 'a t -> position -> bool
+(** [inside g p] indicates whether position [p] is a legal position in [g] *)
+
+val north : position -> position
+(** the position above in the grid *)
+
+val north_west : position -> position
+(** the position above left in the grid *)
+
+val west : position -> position
+(** the position to the left in the grid *)
+
+val south_west : position -> position
+(** the position below left in the grid *)
+
+val south : position -> position
+(** the position below in the grid *)
+
+val south_east : position -> position
+(** the position below right in the grid *)
+
+val east : position -> position
+(** the position to the right in the grid *)
+
+val north_east : position -> position
+(** the position above right in the grid *)
+
+(** the eight ways to move on the grid *)
+type direction =
+  | N  (** North *)
+  | NW  (** Northwest *)
+  | W  (** West *)
+  | SW  (** Southwest *)
+  | S  (** South *)
+  | SE  (** Southeast *)
+  | E  (** East *)
+  | NE  (** Northeast *)
+
+val move : direction -> position -> position
+(** move a position in a given direction *)
+
+val rotate_left : 'a t -> 'a t
+(** [rotate_left g] returns a new grid that is the left rotation of [g] *)
+
+val rotate_right : 'a t -> 'a t
+(** [rotate_right g] returns a new grid that is the right rotation of [g] *)
+
+val map : (position -> 'a -> 'b) -> 'a t -> 'b t
+(** [map f g] returns a fresh grid, with the size of the grid [g] and where the
+    value at position [p] is given by [f p (get g p)] *)
+
+(** {e The following functions that iterate or fold over the neighbors of [p]
+       all begin by calling [f] on the cell north of [p] and rotate clockwise.}
+*)
+
+val iter4 : (position -> 'a -> unit) -> 'a t -> position -> unit
+(** [iter4 f g p] applies function [f] on the four neighbors of position [p]
+    (provided they exist) *)
+
+val iter8 : (position -> 'a -> unit) -> 'a t -> position -> unit
+(** [iter8 f g p] applies function [f] on the eight neighbors of position [p]
+    (provided they exist) *)
+
+val fold4 : (position -> 'a -> 'acc -> 'acc) -> 'a t -> position -> 'acc -> 'acc
+(** [fold4 f g p] folds function [f] on the four neighbors of position [p]
+    (provided they exist) *)
+
+(** [fold8 f g p] folds function [f] on the eight neighbors of position [p]
+    (provided they exist) *)
+
+val fold8 : (position -> 'a -> 'acc -> 'acc) -> 'a t -> position -> 'acc -> 'acc
+(** {e [iter] and [fold] both begin at the top left of the grid and move left to
+       right in each row from top to bottom.} *)
+
+val iter : (position -> 'a -> unit) -> 'a t -> unit
+(** [iter f g] applies function [f] at each position of the grid [g] *)
+
+val fold : (position -> 'a -> 'acc -> 'acc) -> 'a t -> 'acc -> 'acc
+(** [fold f g] folds function [f] over each position of the grid [g] *)
+
+val find : (position -> 'a -> bool) -> 'a t -> position
+(** [find f g] returns a position in [g] where [f] holds, or raises [Not_found]
+    if there is none *)
+
+val print :
+  ?bol:(Format.formatter -> int -> unit) ->
+  ?eol:(Format.formatter -> int -> unit) ->
+  ?sep:(Format.formatter -> position -> unit) ->
+  (Format.formatter -> position -> 'a -> unit) ->
+  Format.formatter ->
+  'a t ->
+  unit
+(** [print pp fmt g] prints the grid [g] on formatter [fmt], using function [pp]
+    to print each element.
+
+    Function [bol] is called at the beginning of each line, and is passed the
+    line number. The default function does nothing.
+
+    Function [eol] is called at the end of each line, and is passed the line
+    number. The default function calls [Format.pp_print_newline].
+
+    Function [sep] is printed between two consecutive element on a given row
+    (and is passed the position of the left one). The default function does
+    nothing. *)
+
+val print_chars : Format.formatter -> char t -> unit
+(** prints a grid of characters using [Format.pp_print_char] *)
+
+val read : in_channel -> char t
+(** [read c] reads a grid of characters from the input channel [c]. Raises
+    [Invalid_argument] if the lines do not have the same length, or there is no
+    line at all. *)
