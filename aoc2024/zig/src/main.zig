af8f2d0327633bcef8cbb7fbca95d2190d76b677
roy.crippen4 <roy.crippen4@archarithms.com>
2025-03-20 23:22:56 -0400

    fix(zig2024): fix

--- aoc2024/zig/src/main.zig
+++ aoc2024/zig/src/main.zig
@@ -1,11 +1,6 @@
 const std = @import("std");
-const input = @import("myfile");
-// const day01 = @import("day01.zig");
+const day01 = @import("day01.zig");
 
 pub fn main() !void {
-    // for (input) |line| {
-    //     std.debug.print("{s}\n", .{line});
-    // }
-    // const answer = try day01.part1();
-    // std.debug.print("Answer: {}\n", .{answer});
+    _ = try day01.part1();
 }
