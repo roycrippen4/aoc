ac111f32991588316eba7cb7e14202152f8a6efe
roy.crippen4 <roy.crippen4@archarithms.com>
2025-04-16 20:54:30 -0400

    feat(ocaml2024): day 10 started

--- aoc2024/aoc_ocaml/lib/day10.ml
+++ aoc2024/aoc_ocaml/lib/day10.ml
@@ -1,7 +1,76 @@
 open Util
 
-let input = read_to_string "/home/roy/dev/aoc/aoc2024/data/day10/data.txt"
+type point = { x : int; y : int; v : int }
+type grid = int Grid.t
+
+module Visited = Set.Make (struct
+  type t = int * int
+
+  let compare = compare
+end)
+
+let grid =
+  "/home/roy/dev/aoc/aoc2024/data/day10/data.txt"
+  |> read_to_string
+  |> String.trim
+  |> Grid.of_string
+  |> Grid.map (fun _ c -> int_of_char2 c)
+
+let gheight = Grid.height grid
+let gwidth = Grid.width grid
+
+let show_grid () =
+  let rec print_row = function
+    | [] -> Printf.printf "\n"
+    | hd :: tl ->
+        Printf.printf "%d" hd;
+        print_row tl
+  in
+
+  let rec print_rows = function
+    | [] -> ()
+    | row :: rest ->
+        print_row row;
+        print_rows rest
+  in
+
+  grid |> Grid.to_list |> print_rows
+
+let () = show_grid ()
 let solve1 () = 42
+
+let get_neighbors p g =
+  let get_neighbor x y = { x; y; v = Grid.get g (y, x) } in
+
+  let { x; y; v } = p in
+  let target = v + 1 in
+  let neighbors = [] in
+
+  let neighbors =
+    if x < gwidth - 1 && Grid.get g (y, x) = target then
+      get_neighbor (x + 1) y :: neighbors
+    else neighbors
+  in
+
+  let neighbors =
+    if x <> 0 && Grid.get g (y, x - 1) = target then
+      get_neighbor (x - 1) y :: neighbors
+    else neighbors
+  in
+
+  let neighbors =
+    if y <> 0 && Grid.get g (y - 1, x) = target then
+      get_neighbor x (y - 1) :: neighbors
+    else neighbors
+  in
+
+  let neighbors =
+    if y < gheight - 1 && Grid.get g (y + 1, x) = target then
+      get_neighbor x (y + 1) :: neighbors
+    else neighbors
+  in
+
+  neighbors
 
 (* part 2 *)
 
