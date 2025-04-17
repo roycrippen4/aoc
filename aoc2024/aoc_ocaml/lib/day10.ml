bce1fa684c5d106cc63c673255699e247563d285
roy.crippen4 <roy.crippen4@archarithms.com>
2025-04-17 00:50:15 -0400

    feat(ocaml2024): day 10 both parts complete

--- aoc2024/aoc_ocaml/lib/day10.ml
+++ aoc2024/aoc_ocaml/lib/day10.ml
@@ -18,106 +18,61 @@ let grid = create_grid input
 let gheight = G.height grid
 let gwidth = G.width grid
 
-let show_grid g =
-  let rec print_row = function
-    | [] -> Printf.printf "\n"
-    | hd :: tl ->
-        Printf.printf "%d" hd;
-        print_row tl
-  in
-
-  let rec print_rows = function
-    | [] -> ()
-    | row :: rest ->
-        print_row row;
-        print_rows rest
-  in
-
-  g |> G.to_list |> print_rows
-
-let get_neighbors p g =
+let get_neighbors p =
   let point_of_entry = function
     | Some (y, x, v) -> if v = p.v + 1 then Some { x; y; v } else None
     | None -> None
   in
-  G.neighbor4_entries g (p.y, p.x) |> List.filter_map point_of_entry
+  G.neighbor4_entries grid (p.y, p.x) |> List.filter_map point_of_entry
 
-let get_start_points g =
-  g |> G.filter (fun _ v -> v = 0) |> List.map (fun (y, x) -> { x; y; v = 0 })
-
-let score_path p g =
-  let visited = Hashtbl.create 0 in
-
-  let rec aux p =
-    Hashtbl.add visited p true;
-    let not_visited p = not (Hashtbl.mem visited p) in
-
-    if p.v = 9 then 1
-    else
-      let ns = get_neighbors p g |> List.filter not_visited in
-      if List.is_empty ns then 0
-      else ns |> List.map aux |> List.fold_left ( + ) 0
-  in
-  aux p
+let start_points =
+  grid
+  |> G.filter (fun _ v -> v = 0)
+  |> List.map (fun (y, x) -> { x; y; v = 0 })
 
 let solve1 () =
-  let starting_points = get_start_points grid in
+  let score_path p =
+    let visited = Hashtbl.create 0 in
 
-  let rec aux acc = function
-    | [] -> acc
-    | p :: ps -> aux (acc + score_path p grid) ps
+    let rec aux p =
+      Hashtbl.add visited p true;
+      let not_visited p = not (Hashtbl.mem visited p) in
+
+      if p.v = 9 then 1
+      else
+        let ns = get_neighbors p |> List.filter not_visited in
+        if List.is_empty ns then 0
+        else ns |> List.map aux |> List.fold_left ( + ) 0
+    in
+    aux p
   in
 
-  aux 0 starting_points
+  let rec solve acc = function
+    | [] -> acc
+    | p :: ps -> solve (acc + score_path p) ps
+  in
+
+  solve 0 start_points
 
 (* part 2 *)
 
-let solve2 () = 42
+let solve2 () =
+  let rec score_path p =
+    if p.v = 9 then 1
+    else p |> get_neighbors |> List.map score_path |> List.fold_left ( + ) 0
+  in
+
+  let rec solve acc = function
+    | [] -> acc
+    | p :: ps -> solve (acc + score_path p) ps
+  in
+
+  solve 0 start_points
 
 (* exports *)
 
 let part1 () = validate solve1 517 "10" One
-let part2 () = validate solve2 42 "10" Two
+let part2 () = validate solve2 1116 "10" Two
 let solution : solution = { part1; part2 }
 
 (* tests *)
-
-let%test _ =
-  let ns =
-    String.trim {|
-9990999
-9991999
-9992999
-6543456
-7999997
-8111118
-9111119
-|}
-    |> create_grid
-    |> get_neighbors { x = 3; y = 0; v = 0 }
-  in
-  let len_is_one = List.length ns = 1 in
-  let n_is_same = List.hd ns = { x = 3; y = 1; v = 1 } in
-  len_is_one && n_is_same
-
-let%test _ =
-  let eq a b = a = b in
-  {|
-9990999
-9991999
-9992999
-6543456
-7999997
-8111118
-9111119
-|}
-  |> String.trim
-  |> create_grid
-  |> score_path { x = 3; y = 0; v = 0 }
-  |> eq 2
-  && "/home/roy/dev/aoc/aoc2024/data/day10/example.txt"
-     |> read_to_string
-     |> String.trim
-     |> create_grid
-     |> score_path { x = 2; y = 0; v = 0 }
-     |> eq 5
