fb579d05638716af9ca7adad3ae5509b909ec212
roy.crippen4 <roy.crippen4@archarithms.com>
2025-04-18 14:37:45 -0400

    feat(rust2024): simplify day 12 part 1

--- aoc2024/rust/src/day12/part1.rs
+++ aoc2024/rust/src/day12/part1.rs
@@ -1,87 +1,90 @@
+use std::sync::LazyLock;
+
 use crate::DIRECTIONS;
 
-#[derive(Debug, Default)]
-struct Grid {
-    data: Vec<u8>,
-    height: usize,
-    width: usize,
+static _SIZE: LazyLock<usize> = LazyLock::new(|| 5);
+
+fn make_grid(input: &str) -> (usize, Vec<u8>) {
+    let data: Vec<Vec<_>> = input
+        .trim()
+        .split('\n')
+        .map(|s| s.chars().map(|c| c as u8).collect())
+        .collect();
+
+    let size = data.len();
+    let grid: Vec<_> = data.into_iter().flatten().collect();
+
+    (size, grid)
 }
 
-impl Grid {
-    pub fn new(input: &str) -> Self {
-        let data: Vec<Vec<u8>> = input
-            .trim()
-            .split('\n')
-            .map(|s| s.chars().map(|c| c as u8).collect())
-            .collect();
-
-        let height = data.len();
-        let width = data[0].len();
-        let data: Vec<u8> = data.into_iter().flatten().collect();
-
-        Self {
-            height,
-            data,
-            width,
-        }
-    }
-
-    #[inline(always)]
-    pub fn idx(&self, x: usize, y: usize) -> u8 {
-        self.data[self.width * y + x]
-    }
+#[inline(always)]
+fn idx(x: usize, y: usize, size: usize) -> usize {
+    size * y + x
 }
 
-fn evaluate(data: &str) -> usize {
-    let grid = Grid::new(data);
-    let mut visited = vec![false; grid.width * grid.height];
-    let mut total_price = 0;
+fn evaluate(input: &str) -> usize {
+    let (size, grid) = make_grid(input);
+    let mut visited = vec![false; size * size];
 
-    for y in 0..grid.height {
-        for x in 0..grid.width {
-            if visited[y * grid.width + x] {
-                continue;
-            }
+    let xyc = |i: usize| (i % size, i / size, grid[i]);
 
-            let current = grid.idx(x, y);
-            let mut area = 0;
-            let mut perimiter = 0;
-            let mut q = Vec::from([(x, y)]);
-            visited[y * grid.width + x] = true;
-
-            while let Some((cx, cy)) = q.pop() {
-                area += 1;
-
-                for &(dx, dy) in &DIRECTIONS {
-                    let nx = cx as isize + dx;
-                    let ny = cy as isize + dy;
-                    let out_of_bounds =
-                        nx < 0 || nx >= grid.width as isize || ny < 0 || ny >= grid.height as isize;
-
-                    if out_of_bounds {
-                        perimiter += 1;
-                        continue;
-                    }
-
-                    let nx = nx as usize;
-                    let ny = ny as usize;
-
-                    if grid.idx(nx, ny) != current {
-                        perimiter += 1;
-                    } else {
-                        let idx = ny * grid.width + nx;
-                        if !visited[idx] {
-                            visited[idx] = true;
-                            q.push((nx, ny));
-                        }
-                    }
-                }
-            }
-            total_price += area * perimiter
+    (0..grid.len()).fold(0, |acc, i| {
+        if visited[i] {
+            acc
+        } else {
+            acc + walk_area(xyc(i), (size, &grid), &mut visited)
         }
+    })
+}
+
+fn walk_area(
+    (x, y, c): (usize, usize, u8),
+    (size, grid): (usize, &[u8]),
+    visited: &mut [bool],
+) -> usize {
+    let mut area = 0;
+    let mut perimeter = 0;
+    let mut q = Vec::from([(x, y)]);
+    visited[y * size + x] = true;
+
+    while let Some((cx, cy)) = q.pop() {
+        area += 1;
+        walk_perimeter((cx, cy, c), (size, grid), &mut q, &mut perimeter, visited);
     }
 
-    total_price
+    area * perimeter
+}
+
+fn walk_perimeter(
+    (cx, cy, c): (usize, usize, u8),
+    (size, grid): (usize, &[u8]),
+    queue: &mut Vec<(usize, usize)>,
+    perimeter: &mut usize,
+    visited: &mut [bool],
+) {
+    let cx_i = cx as isize;
+    let cy_i = cy as isize;
+    let size_i = size as isize;
+
+    for &(dx, dy) in &DIRECTIONS {
+        let nx_i = cx_i + dx;
+        let ny_i = cy_i + dy;
+
+        if nx_i < 0 || nx_i >= size_i || ny_i < 0 || ny_i >= size_i {
+            *perimeter += 1;
+            continue;
+        }
+
+        let nx = nx_i as usize;
+        let ny = ny_i as usize;
+        let idx = idx(nx, ny, size);
+        if grid[idx] != c {
+            *perimeter += 1;
+        } else if !visited[idx] {
+            visited[idx] = true;
+            queue.push((nx, ny));
+        }
+    }
 }
 
 pub fn solve() -> usize {
