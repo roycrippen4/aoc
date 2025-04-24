f74e3fac3372aaf42a0d81f223e115bfd4416b8f
roy.crippen4 <roy.crippen4@archarithms.com>
2025-04-23 20:01:09 -0400

    feat(ocaml2024): day 15 both parts complete

--- aoc2024/aoc_ocaml/lib/day15.ml
+++ aoc2024/aoc_ocaml/lib/day15.ml
@@ -2,8 +2,9 @@ open Util
 module G = Grid
 module String = Batteries.String
 
+(* BoxEnd is only used in part 2 *)
+type kind = Bot | Start | End | Empty | Wall
 type direction = Up | Down | Left | Right
-type kind = Bot | Box | Empty | Wall
 type bot = { mutable x : int; mutable y : int }
 
 let string_of_grid s =
@@ -11,7 +12,7 @@ let string_of_grid s =
   |> G.of_string
   |> G.map_values (function
        | '@' -> Bot
-       | 'O' -> Box
+       | 'O' -> Start
        | '.' -> Empty
        | '#' -> Wall
        | _ -> assert false)
@@ -28,83 +29,58 @@ let string_of_directions s =
        | '>' -> Right
        | _ -> assert false)
 
+let ( <$> ) f g (x, y) = (f x, g y)
 let input = "/home/roy/dev/aoc/aoc2024/data/day15/data.txt" |> read_to_string
 
 let grid, directions =
-  let ( <$> ) f g (x, y) = (f x, g y) in
   input
   |> String.trim
   |> String.split ~by:"\n\n"
   |> map_tuple String.trim
   |> (string_of_grid <$> string_of_directions)
 
-let is_bot _ k = k = Bot
-let set_bot (x, y, _) = { x; y }
-let bot = G.find_replace is_bot Empty grid |> set_bot
+let get_bot grid =
+  G.find_replace (fun _ k -> k = Bot) Empty grid |> fun (x, y, _) -> { x; y }
 
-let rec move_right x =
-  match grid.(bot.y).(x + 1) with
-  | Box -> move_right (x + 1)
+let bot = get_bot grid
+
+let rec move_horiz x offset =
+  match grid.(bot.y).(x + offset) with
+  | Start -> move_horiz (x + offset) offset
   | Empty ->
-      bot.x <- bot.x + 1;
+      bot.x <- bot.x + offset;
       grid.(bot.y).(bot.x) <- Empty;
-      grid.(bot.y).(x + 1) <- Box
+      grid.(bot.y).(x + offset) <- Start
   | _ -> ()
 
-let rec move_left x =
-  match grid.(bot.y).(x - 1) with
-  | Box -> move_left (x - 1)
+let rec move_vert y offset =
+  match grid.(y + offset).(bot.x) with
+  | Start -> move_vert (y + offset) offset
   | Empty ->
-      bot.x <- bot.x - 1;
+      bot.y <- bot.y + offset;
       grid.(bot.y).(bot.x) <- Empty;
-      grid.(bot.y).(x - 1) <- Box
-  | _ -> ()
-
-let rec move_down y =
-  match grid.(y + 1).(bot.x) with
-  | Box -> move_down (y + 1)
-  | Empty ->
-      bot.y <- bot.y + 1;
-      grid.(bot.y).(bot.x) <- Empty;
-      grid.(y + 1).(bot.x) <- Box
-  | _ -> ()
-
-let rec move_up y =
-  match grid.(y - 1).(bot.x) with
-  | Box -> move_up (y - 1)
-  | Empty ->
-      bot.y <- bot.y - 1;
-      grid.(bot.y).(bot.x) <- Empty;
-      grid.(y - 1).(bot.x) <- Box
+      grid.(y + offset).(bot.x) <- Start
   | _ -> ()
 
 let next = function
-  | Up -> (
-      match grid.(bot.y - 1).(bot.x) with
-      | Box -> move_up (bot.y - 1)
-      | Empty -> bot.y <- bot.y - 1
+  | (Up | Down) as v -> (
+      let offset = if v = Up then -1 else 1 in
+      match grid.(bot.y + offset).(bot.x) with
+      | Start -> move_vert (bot.y + offset) offset
+      | Empty -> bot.y <- bot.y + offset
       | _ -> ())
-  | Down -> (
-      match grid.(bot.y + 1).(bot.x) with
-      | Box -> move_down (bot.y + 1)
-      | Empty -> bot.y <- bot.y + 1
-      | _ -> ())
-  | Left -> (
-      match grid.(bot.y).(bot.x - 1) with
-      | Box -> move_left (bot.x - 1)
-      | Empty -> bot.x <- bot.x - 1
-      | _ -> ())
-  | Right -> (
-      match grid.(bot.y).(bot.x + 1) with
-      | Box -> move_right (bot.x + 1)
-      | Empty -> bot.x <- bot.x + 1
+  | (Left | Right) as h -> (
+      let offset = if h = Left then -1 else 1 in
+      match grid.(bot.y).(bot.x + offset) with
+      | Start -> move_horiz (bot.x + offset) offset
+      | Empty -> bot.x <- bot.x + offset
       | _ -> ())
 
 (* part1 *)
 
 let solve1 () =
   let accumulate acc = function
-    | x, y, k when k = Box -> (100 * y) + x + acc
+    | x, y, k when k = Start -> (100 * y) + x + acc
     | _ -> acc
   in
   List.iter (fun d -> next d) directions;
@@ -112,94 +88,168 @@ let solve1 () =
 
 (************************** part 2 **************************)
 
-(* type kind2 = Bot | BoxStart | BoxEnd | Empty | Wall *)
-(**)
-(* let transform = function *)
-(*   | '#' -> "##" *)
-(*   | 'O' -> "[]" *)
-(*   | '.' -> ".." *)
-(*   | '@' -> "@." *)
-(*   | '\n' -> "\n" *)
-(*   | _ -> assert false *)
-(**)
-(* let kind_of_char = function *)
-(*   | '@' -> Bot *)
-(*   | '[' -> BoxStart *)
-(*   | ']' -> BoxEnd *)
-(*   | '.' -> Empty *)
-(*   | '#' -> Wall *)
-(*   | _ -> assert false *)
-(**)
-(* let char_of_kind = function *)
-(*   | Bot -> '@' *)
-(*   | BoxStart -> '[' *)
-(*   | BoxEnd -> ']' *)
-(*   | Empty -> '.' *)
-(*   | Wall -> '#' *)
-(**)
-(* let string_of_grid s = *)
-(*   let buf = Buffer.create (String.length s * 2) in *)
-(*   s |> String.iter (fun c -> Buffer.add_string buf (transform c)); *)
-(*   Buffer.contents buf |> G.of_string |> G.map_values kind_of_char *)
-(**)
-(* let parse str = *)
-(*   str *)
-(*   |> String.trim *)
-(*   |> Batteries.String.split ~by:"\n\n" *)
-(*   |> map_tuple String.trim *)
-(*   |> (string_of_grid <$> string_of_directions) *)
-(**)
-(* let pp_kind fmt _ k = fprintf fmt "%c" (char_of_kind k) *)
-(* let show_grid grid = G.print pp_kind std_formatter grid *)
+let string_of_grid s =
+  let buf = Buffer.create (String.length s * 2) in
 
-(* *)
+  let add_to_buf = function
+    | '#' -> Buffer.add_string buf "##"
+    | 'O' -> Buffer.add_string buf "[]"
+    | '.' -> Buffer.add_string buf ".."
+    | '@' -> Buffer.add_string buf "@."
+    | '\n' -> Buffer.add_string buf "\n"
+    | _ -> assert false
+  in
 
-(* let example_input = *)
-(*   "#######\n#...#.#\n#.....#\n#..OO@#\n#..O..#\n#.....#\n#######\n\n<vv<<^^<<^^" *)
-(**)
-(* let grid, directions = parse example_input *)
-(* let width = G.width grid *)
-(* let bot = G.find (fun _ k -> k = Bot) grid |> fun (x, y) -> { x; y } *)
-(* let show_bot () = printf "bot_x: %d, bot_y: %d\n" bot.x bot.y *)
+  String.iter add_to_buf s;
+
+  Buffer.contents buf
+  |> G.of_string
+  |> G.map_values (function
+       | '@' -> Bot
+       | '[' -> Start
+       | ']' -> End
+       | '.' -> Empty
+       | '#' -> Wall
+       | _ -> assert false)
+
+let parse str =
+  str
+  |> String.trim
+  |> Batteries.String.split ~by:"\n\n"
+  |> map_tuple String.trim
+  |> (string_of_grid <$> string_of_directions)
+
+let pp_kind fmt _ k =
+  Format.fprintf fmt "%c"
+    (match k with
+    | Bot -> '@'
+    | Start -> '['
+    | End -> ']'
+    | Empty -> '.'
+    | Wall -> '#')
+
+let show_grid grid bot =
+  grid.(bot.y).(bot.x) <- Bot;
+  G.print pp_kind Format.std_formatter grid;
+  grid.(bot.y).(bot.x) <- Empty;
+  print_endline ""
+
+let g, directions = parse input
+let width = G.width g
+let height = G.height g
+let bot = get_bot g
+let is_wall x y = g.(y).(x) = Wall
+let in_bounds x y = G.inside g (x, y)
+
+let move_right x =
+  let rec shift_right x =
+    if x = bot.x then bot.x <- bot.x + 1
+    else (
+      g.(bot.y).(x) <- g.(bot.y).(x - 1);
+      shift_right (x - 1))
+  in
+
+  let rec aux x =
+    match g.(bot.y).(x) with
+    | Start -> aux (x + 2)
+    | Empty -> shift_right x
+    | _ -> ()
+  in
+
+  aux x
+
+let move_left x =
+  let rec shift_left x =
+    if x = bot.x then bot.x <- bot.x - 1
+    else (
+      g.(bot.y).(x) <- g.(bot.y).(x + 1);
+      shift_left (x + 1))
+  in
+
+  let rec aux x =
+    match g.(bot.y).(x) with
+    | End -> aux (x - 2)
+    | Empty -> shift_left x
+    | _ -> ()
+  in
+
+  aux x
+
+let false_array = fun _ -> Array.init width (fun _ -> false)
+let make_seen () = Array.init height false_array
+
+let move_vert x y up =
+  let dy = if up then -1 else 1 in
+  let seen = make_seen () in
+
+  let rec get_moves x y =
+    match g.(y).(x) with
+    | (Start | End) as k ->
+        let ox = if k = Start then x + 1 else x - 1 in
+        if seen.(y).(ox) then Some []
+        else (
+          seen.(y).(x) <- true;
+          seen.(y).(ox) <- true;
+          let ty = y + dy in
+
+          let* l1 =
+            match g.(ty).(x) with
+            | Start | End -> get_moves x ty
+            | Empty -> Some []
+            | _ -> None
+          in
+
+          let* l2 =
+            match g.(ty).(ox) with
+            | Start | End -> get_moves ox ty
+            | Empty -> Some []
+            | _ -> None
+          in
+
+          Some (l1 @ l2 @ [ (x, y, k); (ox, y, g.(y).(ox)) ]))
+    | _ -> None
+  in
+
+  let apply_move (x, y, k) =
+    g.(y).(x) <- Empty;
+    g.(y + dy).(x) <- k
+  in
+
+  match get_moves x y with
+  | None -> ()
+  | Some moves ->
+      moves |> List.iter apply_move;
+      bot.y <- y
+
+let next d =
+  match d with
+  | Left -> (
+      match g.(bot.y).(bot.x - 1) with
+      | End -> move_left (bot.x - 3)
+      | Empty -> bot.x <- bot.x - 1
+      | _ -> ())
+  | Right -> (
+      match g.(bot.y).(bot.x + 1) with
+      | Start -> move_right (bot.x + 3)
+      | Empty -> bot.x <- bot.x + 1
+      | _ -> ())
+  | (Up | Down) as d -> (
+      let up = d = Up in
+      let dy = if up then bot.y - 1 else bot.y + 1 in
+
+      match g.(dy).(bot.x) with
+      | Start | End -> move_vert bot.x dy up
+      | Empty -> bot.y <- dy
+      | _ -> ())
 
 let solve2 () =
-  (* show_grid grid; *)
-  (* next Left; *)
-  (* show_grid grid; *)
-  (* let accumulate acc = function *)
-  (*   | x, y, k when k = BoxStart -> (100 * y) + x + acc *)
-  (*   | _ -> acc *)
-  (* in *)
-  (* List.iter (fun d -> next d) directions; *)
-  (* grid |> G.fold accumulate 0 *)
-  42
+  List.iter next directions;
+  g
+  |> G.filter_entries (fun (_, _, v) -> v = Start)
+  |> List.fold_left (fun acc (x, y, _) -> acc + (100 * y) + x) 0
 
 (* exports *)
 
 let part1 () = validate solve1 1526673 "15" One
-let part2 () = validate solve2 42 "15" Two
+let part2 () = validate solve2 1535509 "15" Two
 let solution : solution = { part1; part2 }
-
-(* tests *)
-
-let%test _ =
-  string_of_directions "<^^>>>vv<v>>v<<"
-  = [
-      Left;
-      Up;
-      Up;
-      Right;
-      Right;
-      Right;
-      Down;
-      Down;
-      Left;
-      Down;
-      Right;
-      Right;
-      Down;
-      Left;
-      Left;
-    ]
-
-(* [ Left; Down; Down; Left; Left; Up; Up; Left; Left; Up; Up ] = direction *)
