f10b0ca63710be154f45107e87bf73580c31d975
roy.crippen4 <roy.crippen4@archarithms.com>
2025-05-05 00:03:42 -0400

    feat(ocaml2024): day 18 part 2 complete (slow)

--- aoc2024/aoc_ocaml/lib/day18.ml
+++ aoc2024/aoc_ocaml/lib/day18.ml
@@ -20,38 +20,48 @@ end
 
 let show_grid grid = Grid.print Kind.pp_kind Format.std_formatter grid
 
-let grid =
-  let open String in
+let points =
   let ( >> ) f g x = g (f x) in
-
-  let size = 71 in
-  let n_walls = 1024 in
-  let g = Grid.init size size (fun _ -> Kind.Empty) in
-
+  let open String in
   "/home/roy/dev/aoc/aoc2024/data/day18/data.txt"
   |> read_to_string
   |> trim
   |> split_on_char '\n'
   |> List.map (split ~by:"," >> map_tuple int_of_string >> Point.of_tuple)
-  |> List.take n_walls
-  |> List.iter (fun p -> g.Grid.%{p} <- Kind.Wall);
+
+let n_walls = 1024
+
+let grid =
+  let size = 71 in
+  let g = Grid.init size size (fun _ -> Kind.Empty) in
+  points |> List.take n_walls |> List.iter (fun p -> g.Grid.%{p} <- Kind.Wall);
   g
 
+let start = Point.{ x = 0; y = 0 }
+let goal = Point.{ x = 70; y = 70 }
+
+module Dijkstra = Grid.Dijkstra (Kind)
+
 let solve1 () =
-  let module Dijkstra = Grid.Dijkstra (Kind) in
-  Dijkstra.walk grid { x = 0; y = 0 } { x = 70; y = 70 }
-  |> Option.get
-  |> List.length
-  |> pred
+  Dijkstra.walk grid start goal |> Option.get |> List.length |> pred
 
 (* part 2 *)
+let solve2 () =
+  let points = Array.of_list points in
 
-let solve2 () = 42
+  let rec loop idx =
+    grid.Grid.%{points.(idx)} <- Wall;
+    match Dijkstra.walk grid start goal with
+    | Some _ -> loop (idx + 1)
+    | None -> points.(idx)
+  in
+  let point = loop n_walls in
+  point.x * point.y
 
 (* exports *)
 
 let part1 () = validate solve1 506 "18" One
-let part2 () = validate solve2 42 "18" Two
+let part2 () = validate solve2 372 "18" Two
 let solution : solution = { part1; part2 }
 
 (* tests *)
