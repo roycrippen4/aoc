7480c2115fa5393f06e657f86647bb7a10b9ce63
roy.crippen4 <roy.crippen4@archarithms.com>
2025-05-17 03:16:44 -0400

    feat(ocaml2024): day 24 both parts done
    part 2 is really slow. Needs to be fixed

--- aoc2024/aoc_ocaml/lib/day24.ml
+++ aoc2024/aoc_ocaml/lib/day24.ml
@@ -8,6 +8,8 @@ module H = struct
     let tbl = create (List.length lst) in
     List.iter (Tuple.uncurry (add tbl)) lst;
     tbl
+
+  let keys tbl = to_seq_keys tbl |> List.of_seq
 end
 
 module Op = struct
@@ -25,149 +27,152 @@ module Op = struct
     match op with OR -> a lor b | XOR -> a lxor b | AND -> a land b
 end
 
-type instruction = { a : string; b : string; op : Op.t }
+module Expr = struct
+  type t = { lhs : string; rhs : string; op : Op.t }
 
-module Instruction = struct
-  type t = instruction
-
-  let to_string { a; b; op } = Printf.sprintf "%s %s %s" a (Op.to_string op) b
+  let to_string { lhs; rhs; op } =
+    Printf.sprintf "%s %s %s" lhs (Op.to_string op) rhs
 
   let of_list = function
-    | [ a; op; b ] -> { a; op = Op.of_string op; b }
+    | [ lhs; op; rhs ] -> { lhs; op = Op.of_string op; rhs }
     | _ -> assert false
 
   let of_string s = S.split_on_char ' ' s |> of_list
   let parse_fst tup = Tuple.map_fst of_string tup |> Tuple.swap
+  let to_list expr = List.sort compare [ expr.lhs; expr.rhs ]
 end
-
-(** [bfw XOR mjb -> z00] where [z00] is the value derived from computing
-    [bfw XOR mjb] *)
-module Expr = struct
-  type t = string * Instruction.t
-
-  let of_string =
-    S.split ~by:" -> " >> Tuple.swap >> Tuple.map_snd Instruction.of_string
-
-  let to_string (label, instruction) =
-    Printf.printf "%s = %s\n" label (Instruction.to_string instruction)
-end
-
-let partition_zs = function
-  | a, b when a.[0] <> 'z' -> Either.Left (a, b)
-  | a, b -> Either.Right (a, b)
-
-let cmp = fun a b -> S.compare (fst a) (fst b)
-let get_zs = fun z -> List.sort cmp z |> List.map snd
 
 let parse_undefined ls =
-  let split_map s = S.split ~by:" -> " s |> Instruction.parse_fst in
-
-  ls
-  |> List.partition_map (fun s -> split_map s |> partition_zs)
-  |> Tuple.bimap ~f1:H.of_list ~f2:get_zs
+  let split_map s = S.split ~by:" -> " s |> Expr.parse_fst in
+  ls |> List.map (fun s -> split_map s) |> H.of_list
 
 let parse_defined ls =
   List.map (S.split ~by:": " >> Tuple.map_snd int_of_string) ls |> H.of_list
 
-let defined, (undefined, zs) =
-  read_to_string "/home/roy/dev/aoc/aoc2024/data/day24/example.txt"
+let defined, operations =
+  read_to_string "/home/roy/dev/aoc/aoc2024/data/day24/data.txt"
   |> S.trim |> S.split ~by:"\n\n" |> Tuple.map S.lines
   |> Tuple.bimap ~f1:parse_defined ~f2:parse_undefined
 
-(*
-
-// Defined
-x00 = 1
-x01 = 0
-x02 = 1
-x03 = 1
-x04 = 0
-y00 = 1
-y01 = 1
-y02 = 1
-y03 = 1
-y04 = 1
-
-(key, (arg1, op, arg2))
-
-// Definable
-djm = y00 AND y03
-pbm = y01 AND x02
-tnw = y02 OR  x01
-psh = y03 OR  y00
-nrd = y03 OR  x01
-fgs = y04 OR  y02
-fst = x00 OR  x03
-vdt = x03 OR  x00
-ntg = x00 XOR y04
-ffh = x03 XOR y03
-kjc = x04 AND y00
-
-// 2nd order Definable
-qhw = djm OR  pbm
-kpj = pbm OR  djm
-mjb = ntg XOR fgs
-bfw = vdt OR  tnw
-bqk = ffh OR  nrd
-frj = tnw OR  fst
-hwm = nrd AND vdt
-rvg = kjc AND fst
-kwq = ntg OR  kjc
-tgd = psh XOR fgs
-wpb = nrd XOR fgs
-gnj = tnw OR  pbm
-
-// 3rd order Definable
-z00 = bfw XOR mjb
-z01 = tgd XOR rvg
-z02 = gnj AND wpb
-z03 = hwm AND bqk
-z04 = frj XOR qhw
-z05 = kwq OR  kpj
-z06 = bfw OR  bqk
-z07 = bqk OR  frj
-z08 = bqk OR  frj
-z09 = qhw XOR tgd
-z10 = bfw AND frj
-z11 = gnj AND tgd
-z12 = tgd XOR rvg
-*)
+let zs =
+  let aux k v acc = if k.[0] = 'z' then (k, v) :: acc else acc in
+  let cmp a b = String.compare (fst a) (fst b) in
+  H.fold aux operations [] |> List.sort cmp |> List.map snd
 
 let rec define arg =
-  let open Hashtbl in
-  let eval a b op =
-    let result = Op.apply a op b in
-    add defined arg result;
-    remove undefined arg;
-    result
-  in
-
-  try find defined arg
-  with Not_found -> (
-    let { a; b; op } = find undefined arg in
-
-    match (find_opt defined a, find_opt defined b) with
-    | Some a', Some b' -> eval a' b' op
-    | Some a', None -> eval a' (define b) op
-    | None, Some b' -> eval (define a) b' op
-    | None, None -> eval (define a) (define b) op)
+  match H.find_opt defined arg with
+  | Some value -> value
+  | None ->
+      let { lhs; rhs; op } : Expr.t = H.find operations arg in
+      let result = Op.apply (define lhs) op (define rhs) in
+      H.add defined arg result;
+      result
 
 let solve1 () =
-  let eval { a; b; op } = Op.apply (define a) op (define b) in
-  let result = zs |> List.rev_map eval in
+  let eval (e : Expr.t) i =
+    let bit = Op.apply (define e.lhs) e.op (define e.rhs) in
+    bit * pow 2 i
+  in
 
-  result |> List.iter (fun v -> Printf.printf "%d" v);
-  print_endline "";
-  42
+  let accumulate (i, acc) (expr : Expr.t) = (succ i, acc + eval expr i) in
+  zs |> List.fold_left accumulate (0, 0) |> snd
 
 (* part 2 *)
 
-let solve2 () = 42
+let make_wire c n = Printf.sprintf "%c%02d" c n
+let make_wires n = [ make_wire 'x' n; make_wire 'y' n ]
+
+let is_ok_xor wire n =
+  match H.find_opt operations wire with
+  | None -> false
+  | Some expr when expr.op <> Op.XOR -> false
+  | Some expr -> Expr.to_list expr = make_wires n
+
+let rec is_ok_carry wire n =
+  let is_ok_direct_carry wire n =
+    match H.find_opt operations wire with
+    | None -> false
+    | Some expr when expr.op <> Op.AND -> false
+    | Some expr -> Expr.to_list expr = make_wires n
+  in
+
+  let is_ok_recarry wire n =
+    match H.find_opt operations wire with
+    | None -> false
+    | Some expr when expr.op <> Op.AND -> false
+    | Some expr ->
+        (is_ok_xor expr.lhs n && is_ok_carry expr.rhs n)
+        || (is_ok_xor expr.rhs n && is_ok_carry expr.lhs n)
+  in
+
+  match H.find_opt operations wire with
+  | None -> false
+  | Some expr when n = 1 && expr.op <> Op.AND -> false
+  | Some expr when n = 1 -> Expr.to_list expr = [ "x00"; "y00" ]
+  | Some expr when expr.op <> Op.OR -> false
+  | Some expr ->
+      let n = n - 1 in
+      (is_ok_direct_carry expr.lhs n && is_ok_recarry expr.rhs n)
+      || (is_ok_direct_carry expr.rhs n && is_ok_recarry expr.lhs n)
+
+let is_ok_z wire n =
+  match H.find_opt operations wire with
+  | None -> false
+  | Some expr when expr.op <> Op.XOR -> false
+  | Some expr when n = 0 -> Expr.to_list expr = [ "x00"; "y00" ]
+  | Some expr ->
+      (is_ok_xor expr.lhs n && is_ok_carry expr.rhs n)
+      || (is_ok_xor expr.rhs n && is_ok_carry expr.lhs n)
+
+let progress start =
+  let ok_idx i = not (is_ok_z (make_wire 'z' i) i) in
+  Seq.ints start |> Seq.find ok_idx |> Option.get
+
+let swap_wires a b =
+  let v_a = Hashtbl.find operations a in
+  let v_b = Hashtbl.find operations b in
+  Hashtbl.replace operations a v_b;
+  Hashtbl.replace operations b v_a
+
+let get_swaps n_swaps =
+  let exception Break of int * string list in
+  let wires = H.keys operations in
+
+  let rec loop_inner baseline combos best current =
+    match combos with
+    | (a, b) :: rest ->
+        let curr = List.sort String.compare [ a; b ] in
+        swap_wires a b;
+        let local_best = progress best in
+        if local_best > baseline then raise (Break (max best local_best, curr));
+        swap_wires a b;
+        loop_inner baseline rest best curr (* curr becomes new current *)
+    | [] -> (best, current)
+  in
+
+  let rec loop swaps n best current =
+    if n = 0 then swaps
+    else
+      let baseline = progress best in
+      let cs = combos wires in
+      let best, curr =
+        try loop_inner baseline cs best current with Break (b, c) -> (b, c)
+      in
+      loop (curr :: swaps) (pred n) best curr
+  in
+
+  loop [] n_swaps 0 [ ""; "" ]
+  |> List.flatten
+  |> List.sort_uniq String.compare
+  |> String.concat ","
+
+let solve2 () =
+  get_swaps 4 |> String.fold_left (fun acc c -> acc + int_of_char c) 0
 
 (* exports *)
 
-let part1 () = validate solve1 42 "24" One
-let part2 () = validate solve2 42 "24" Two
+let part1 () = validate solve1 42883464055378 "24" One
+let part2 () = validate solve2 2625 "24" Two
 let solution : solution = { part1; part2 }
 
 (* tests *)
