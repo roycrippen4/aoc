f8a2dc7ff3f227963420f7360cf5371cbf4e1ee3
roy.crippen4 <roy.crippen4@archarithms.com>
2025-11-19 14:07:55 -0500

    chore(zig2024): make day03 match other days' formatting

--- aoc2024/zig/src/day03.zig
+++ aoc2024/zig/src/day03.zig
@@ -1,10 +1,15 @@
-const aoc = @import("aoc");
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const testing = std.testing;
+const mem = std.mem;
+
+const aoc = @import("aoc");
+const Solution = aoc.Solution;
 
 const input: []const u8 = @embedFile("data/day03/data.txt");
 
 inline fn parse(s: []const u8) u64 {
-    const comma_idx = std.mem.indexOfScalar(u8, s, ',') orelse return 0;
+    const comma_idx = mem.indexOfScalar(u8, s, ',') orelse return 0;
     const x_slice = s[4..comma_idx];
     const y_slice = s[comma_idx + 1 .. s.len - 1];
     const x = std.fmt.parseInt(u64, x_slice, 10) catch unreachable;
@@ -12,30 +17,30 @@ inline fn parse(s: []const u8) u64 {
     return x * y;
 }
 
-inline fn isMul(s: []const u8) bool {
-    return std.mem.startsWith(u8, s, "mul(");
+inline fn is_mul(s: []const u8) bool {
+    return mem.startsWith(u8, s, "mul(");
 }
 
-inline fn getParenIdx(s: []const u8) usize {
-    return std.mem.indexOfScalar(u8, s, ')') orelse @panic("failed to find a `)`");
+inline fn get_paren_idx(s: []const u8) usize {
+    return mem.indexOfScalar(u8, s, ')') orelse @panic("failed to find a `)`");
 }
 
-inline fn isDo(s: []const u8) bool {
-    return std.mem.startsWith(u8, s, "do()");
+inline fn is_do(s: []const u8) bool {
+    return mem.startsWith(u8, s, "do()");
 }
 
-inline fn isDont(s: []const u8) bool {
-    return std.mem.startsWith(u8, s, "don't()");
+inline fn is_dont(s: []const u8) bool {
+    return mem.startsWith(u8, s, "don't()");
 }
 
-pub fn part1(_: std.mem.Allocator) anyerror!usize {
+pub fn part1(_: Allocator) !usize {
     var i: u64 = 0;
     var sum: u64 = 0;
 
     while (i < input.len) : (i += 1) {
-        if (!isMul(input[i..])) continue;
+        if (!is_mul(input[i..])) continue;
 
-        const j = getParenIdx(input[i..]);
+        const j = get_paren_idx(input[i..]);
         if (j >= 12) continue;
 
         sum += parse(input[i .. i + j + 1]);
@@ -45,27 +50,27 @@ pub fn part1(_: std.mem.Allocator) anyerror!usize {
     return sum;
 }
 
-pub fn part2(_: std.mem.Allocator) anyerror!usize {
+pub fn part2(_: Allocator) !usize {
     var i: u64 = 0;
     var enable = true;
     var sum: u64 = 0;
 
     while (i < input.len) : (i += 1) {
-        if (isDo(input[i..])) {
+        if (is_do(input[i..])) {
             enable = true;
             i += 3;
             continue;
         }
 
-        if (isDont(input[i..])) {
+        if (is_dont(input[i..])) {
             enable = false;
             i += 6;
             continue;
         }
 
-        if (!enable or !isMul(input[i..])) continue;
+        if (!enable or !is_mul(input[i..])) continue;
 
-        const j = getParenIdx(input[i..]);
+        const j = get_paren_idx(input[i..]);
         if (j >= 12) continue;
 
         sum += parse(input[i .. i + j + 1]);
@@ -75,14 +80,12 @@ pub fn part2(_: std.mem.Allocator) anyerror!usize {
     return sum;
 }
 
-const t = std.testing;
-
 test "day03 part1" {
-    _ = try aoc.validate(part1, 173731097, aoc.Day.@"03", aoc.Part.one, t.allocator);
+    _ = try aoc.validate(part1, 173731097, .@"03", .one, testing.allocator);
 }
 
 test "day03 part2" {
-    _ = try aoc.validate(part2, 93729253, aoc.Day.@"03", aoc.Part.two, t.allocator);
+    _ = try aoc.validate(part2, 93729253, .@"03", .two, testing.allocator);
 }
 
 test "day03 parse" {
