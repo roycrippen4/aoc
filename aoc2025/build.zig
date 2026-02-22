49d220f1f2d005bcff12c4049020e3977f0dc32c
roy.crippen4 <roy.crippen4@archarithms.com>
2026-02-21 20:04:57 -0500

    refactor: moved aoc lib to separate repo

--- aoc2025/build.zig
+++ aoc2025/build.zig
@@ -4,17 +4,25 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.addModule("aoc", .{
-        .root_source_file = b.path("src/lib/root.zig"),
+    const aoc_mod = b.dependency("aoc", .{
         .target = target,
+        .optimize = optimize,
     });
+
+    // const lib_mod = b.addModule("aoc", .{
+    //     .root_source_file = b.path("src/lib/root.zig"),
+    //     .target = target,
+    // });
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
         .imports = &.{
-            .{ .name = "aoc", .module = lib_mod },
+            .{
+                .name = "aoc",
+                .module = aoc_mod.module("aoc"),
+            },
         },
     });
 
@@ -40,19 +48,13 @@ pub fn build(b: *std.Build) void {
         "Skip tests that do not match any filter",
     ) orelse &.{};
 
-    const lib_unit_tests = b.addTest(.{ .root_module = lib_mod, .filters = test_filters });
     const exe_unit_tests = b.addTest(.{ .root_module = exe_mod, .filters = test_filters });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 
     const exe_check = b.addExecutable(.{ .name = "aoc2024", .root_module = exe_mod });
-    const lib_check = b.addLibrary(.{ .name = "aoc", .root_module = lib_mod });
     const check = b.step("check", "Check if it compiles");
     check.dependOn(&exe_check.step);
-    check.dependOn(&lib_check.step);
-    check.dependOn(&run_lib_unit_tests.step);
     check.dependOn(&run_exe_unit_tests.step);
 }
