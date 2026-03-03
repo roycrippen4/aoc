b5bf552f37a5c58c0b79129aeed9a500bea6e9a7
roy.crippen4 <roy.crippen4@archarithms.com>
2026-03-02 20:01:16 -0500

    feat(2025): day 10 both parts this one was so difficult to do fast.

--- aoc2025/src/day10.zig
+++ aoc2025/src/day10.zig
@@ -5,136 +5,78 @@ const math = std.math;
 const Allocator = mem.Allocator;
 const testing = std.testing;
 
-const aoc = @import("aoc");
+const aoc = @import("libaoc");
 const Stack = aoc.Stack;
 const Solution = aoc.Solution;
-const BitSet = std.bit_set.StaticBitSet;
 
-const use_example = false;
-const input = if (use_example) example else @embedFile("data/day10.txt");
-
-const example =
-    \\[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-    \\[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-    \\[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-;
-
-fn parse_lights(string: []const u8) u10 {
-    var lights: BitSet(10) = .initEmpty();
-
-    for (string, 0..) |ch, i| if (ch == '#') {
-        lights.set(9 - i);
-    };
-
-    return lights.mask;
-}
-test "day10 parse_lights" {
-    {
-        const result = parse_lights("##.###.##.");
-        try testing.expectEqual(0b1101110110, result);
-    }
-    {
-        const result = parse_lights(".##.");
-        try testing.expectEqual(0b0110000000, result);
-    }
-    {
-        const result = parse_lights("...#.");
-        try testing.expectEqual(0b0001000000, result);
-    }
-}
-
-fn parse_button(string: []const u8) u10 {
-    var i: usize = 0;
-    var button: BitSet(10) = .initEmpty();
-
-    while (i < string.len) : (i += 2) {
-        button.set(9 - (string[i] - '0'));
-    }
-
-    return button.mask;
-}
-test "day10 parse_button" {
-    {
-        const result = parse_button("0,1,4");
-        try testing.expectEqual(0b1100100000, result);
-    }
-    {
-        const result = parse_button("4");
-        try testing.expectEqual(0b0000100000, result);
-    }
-    {
-        const result = parse_button("1,4");
-        try testing.expectEqual(0b0100100000, result);
-    }
-    {
-        const result = parse_button("0,1,2,3,4,5,6,7,8,9");
-        try testing.expectEqual(0b1111111111, result);
-    }
-}
-
-fn parse_buttons(string: []const u8) Stack(u10, 13) {
-    var buttons: Stack(u10, 13) = .{};
-    var buttons_iterator = mem.tokenizeScalar(u8, string, ' ');
-
-    while (buttons_iterator.next()) |button_string| {
-        const button = parse_button(button_string[1 .. button_string.len - 1]);
-        buttons.push(button);
-    }
-
-    return buttons;
-}
-test "day10 parse_buttons" {
-    const buttons = parse_buttons("(3) (1,3) (2) (2,3) (0,2) (0,1)");
-    const expected: Stack(u10, 13) = .{
-        .items = .{
-            0b0001000000,
-            0b0101000000,
-            0b0010000000,
-            0b0011000000,
-            0b1010000000,
-            0b1100000000,
-        } ++ .{undefined} ** 7,
-        .len = 6,
-        .capacity = 13,
-    };
-    try testing.expectEqualDeep(expected, buttons);
-}
-
-fn parse_joltages(string: []const u8) Stack(u32, 10) {
-    var joltages: Stack(u32, 10) = .{};
-    var it = mem.tokenizeScalar(u8, string, ',');
-
-    while (it.next()) |joltage_slice| {
-        const joltage = fmt.parseInt(u32, joltage_slice, 10) catch unreachable;
-        joltages.push(joltage);
-    }
-
-    return joltages;
-}
-test "day10 parse_joltages" {
-    const joltages = parse_joltages("7,5,12,7,2");
-    const expected: Stack(u32, 10) = .{
-        .items = .{ 7, 5, 12, 7, 2 } ++ .{undefined} ** 5,
-        .len = 5,
-        .capacity = 10,
-    };
-    try testing.expectEqualDeep(expected, joltages);
-}
+const input = @embedFile("data/day10.txt");
 
 const Machine = struct {
-    const Buttons = Stack(u10, 13);
-    const Joltages = Stack(u32, 10);
-
-    lights: u10,
-    buttons: Buttons,
-    joltages: Joltages,
+    buttons: Stack(u16, 13),
+    joltages: Stack(i16, 10),
+    lights: u16,
 
     const Self = @This();
 
-    pub fn min_presses(self: Self) usize {
-        const combinations: usize = math.pow(usize, 2, self.buttons.len);
+    pub fn from_string(string: []const u8) Self {
+        const lights_end = mem.indexOfScalar(u8, string, ']').?;
+        const joltage_start = mem.indexOfScalar(u8, string, '{').?;
+
+        const lights = blk: {
+            var lights: u16 = 0;
+            for (string[1..lights_end], 0..) |ch, i| if (ch == '#') {
+                const mask = @as(u16, 1) << @intCast(i);
+                lights |= mask;
+            };
+
+            break :blk lights;
+        };
+
+        const joltages = blk: {
+            var joltages: Stack(i16, 10) = .{};
+            var it = mem.tokenizeScalar(u8, string[joltage_start + 1 .. string.len - 1], ',');
+            while (it.next()) |joltage_slice| {
+                const joltage = fmt.parseInt(i16, joltage_slice, 10) catch unreachable;
+                joltages.push(joltage);
+            }
+
+            break :blk joltages;
+        };
+
+        const buttons = blk: {
+            var buttons: Stack(u16, 13) = .{};
+            var it = mem.tokenizeScalar(u8, string[lights_end + 2 .. joltage_start - 1], ' ');
+            while (it.next()) |button_string| {
+                const button = inner: {
+                    const s = button_string[1 .. button_string.len - 1];
+                    var i: usize = 0;
+                    var button: u16 = 0;
+
+                    // `i += 2` to skip commas
+                    while (i < s.len) : (i += 2) {
+                        const mask = @as(u16, 1) << @intCast((s[i] - '0'));
+                        button |= mask;
+                    }
+
+                    break :inner button;
+                };
+
+                buttons.push(button);
+            }
+            break :blk buttons;
+        };
+
+        return .{
+            .lights = lights,
+            .buttons = buttons,
+            .joltages = joltages,
+        };
+    }
+
+    pub fn configure_buttons(self: Self) usize {
+        const combinations = math.pow(usize, 2, self.buttons.len);
         var min_pressed: usize = math.maxInt(usize);
-        var current_lights: u10 = 0;
+        var current_lights: u16 = 0;
 
         for (1..combinations) |i| {
             current_lights ^= self.buttons.items[@ctz(i)];
@@ -146,54 +88,43 @@ const Machine = struct {
 
         return min_pressed;
     }
-    test "day10 Machine.min_presses" {
-        {
-            const machine: Machine = .from_string("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}");
-            const result = machine.min_presses();
-            try testing.expectEqual(2, result);
+
+    pub fn into_equations(self: Self) [MAX_JOLTAGES][MAX_BUTTONS]i32 {
+        const buttons = self.buttons;
+        const joltages = self.joltages;
+        const width = buttons.len;
+        const height = joltages.len;
+
+        var equations: [MAX_JOLTAGES][MAX_BUTTONS]i32 = .{.{0} ** MAX_BUTTONS} ** MAX_JOLTAGES;
+
+        for (0..height) |row| {
+            equations[row][width] = joltages.items[row];
         }
-        {
-            const machine: Machine = .from_string("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}");
-            const result = machine.min_presses();
-            try testing.expectEqual(3, result);
+
+        for (0..width) |col| {
+            var limit: i32 = math.maxInt(i32);
+            var it = biterator(u16, buttons.items[col]);
+            while (it.next()) |row| {
+                equations[row][col] = 1;
+                limit = @min(limit, joltages.items[row]);
+            }
+            equations[height][col] = limit;
         }
+
+        return equations;
     }
 
-    pub fn from_string(string: []const u8) Self {
-        const lights_end = mem.indexOfScalar(u8, string, ']').?;
-        const joltage_start = mem.indexOfScalar(u8, string, '{').?;
+    pub fn configure_joltages(self: Self) usize {
+        const subspace: Subspace = .from_machine(self);
+        const particular_solution = subspace.solution();
 
-        const lights_string = string[1..lights_end];
-        const buttons_string = string[lights_end + 2 .. joltage_start - 1];
-        const joltages_string = string[joltage_start + 1 .. string.len - 1];
-
-        return .{
-            .lights = parse_lights(lights_string),
-            .buttons = parse_buttons(buttons_string),
-            .joltages = parse_joltages(joltages_string),
-        };
-    }
-
-    pub fn format(self: Self, writer: *std.io.Writer) std.io.Writer.Error!void {
-        try writer.print(
-            \\Machine{{
-            \\    .lights = .{b:0>10},
-            \\    .joltages = .{any},
-            \\    .buttons = .{{
-            \\
-        , .{
-            self.lights,
-            self.joltages.to_slice(),
-        });
-
-        for (self.buttons.to_slice()) |button| {
-            try writer.print("        {b:0>10},\n", .{button});
+        if (subspace.nullity == 0) {
+            return @intCast(@divTrunc(particular_solution, subspace.lcm));
+        } else {
+            const remaining = (@as(u32, 1) << @intCast(subspace.bases.len)) - 1;
+            const possible_value = recurse(subspace, subspace.rhs, remaining, particular_solution).?;
+            return @intCast(possible_value);
         }
-
-        try writer.print(
-            \\    }},
-            \\}}
-        , .{});
     }
 };
 
@@ -213,19 +144,326 @@ const MACHINES: [LINE_COUNT]Machine = blk: {
 fn part1(_: Allocator) !usize {
     var result: usize = 0;
     for (MACHINES) |machine| {
-        result += machine.min_presses();
+        result += machine.configure_buttons();
     }
     return result;
 }
 
+const MAX_BUTTONS: usize = 14;
+const MAX_JOLTAGES: usize = 11;
+
+const Basis = struct {
+    limit: i32,
+    cost: i32,
+    vs: [MAX_JOLTAGES]i32,
+};
+
+const Subspace = struct {
+    rank: usize,
+    nullity: usize,
+    lcm: i32,
+    rhs: [MAX_JOLTAGES]i32,
+    bases: Stack(Basis, 4),
+    const Self = @This();
+
+    pub fn from_machine(machine: Machine) Self {
+        const width = machine.buttons.len;
+        const height = machine.joltages.len;
+        var equations = machine.into_equations();
+        var rank: usize = 0;
+        var last = width;
+
+        while (rank < height and rank < last) {
+            const found = find_swap_index(rank, height, equations) orelse {
+                last -= 1;
+                for (0..height + 1) |index| {
+                    mem.swap(i32, &equations[index][rank], &equations[index][last]);
+                }
+                continue;
+            };
+            mem.swap([MAX_BUTTONS]i32, &equations[rank], &equations[found]);
+
+            var pivot: i32 = equations[rank][rank];
+            if (pivot < 0) {
+                pivot *= -1;
+                for (rank..width + 1) |index| {
+                    equations[rank][index] *= -1;
+                }
+            }
+
+            for (0..height) |row| {
+                const coefficient = equations[row][rank];
+                if (row != rank and coefficient != 0) {
+                    const abs_coefficient: i32 = @intCast(@abs(coefficient));
+                    const lcm = least_common_multiple(abs_coefficient, pivot);
+                    const x = @divFloor(lcm, abs_coefficient);
+                    const sign = math.sign(coefficient);
+                    const y = @divFloor(lcm, pivot * sign);
+
+                    for (0..equations[row].len) |col| {
+                        const value = x * equations[row][col] - y * equations[rank][col];
+                        equations[row][col] = value;
+                    }
+                }
+            }
+            rank += 1;
+        }
+
+        var lcm: i32 = 1;
+        for (0..rank) |pivot| {
+            lcm = least_common_multiple(lcm, equations[pivot][pivot]);
+        }
+
+        var pivot: usize = 0;
+        for (0..rank) |i| {
+            const q = @divFloor(lcm, equations[i][pivot]);
+            for (rank..width + 1) |j| {
+                equations[i][j] *= q;
+            }
+            pivot += 1;
+        }
+
+        const nullity = width - rank;
+        const rhs = blk: {
+            var rhs: [MAX_JOLTAGES]i32 = undefined;
+            for (0..MAX_JOLTAGES) |index| {
+                rhs[index] = equations[index][width];
+            }
+            break :blk rhs;
+        };
+
+        var bases: Stack(Basis, 4) = .{};
+        for (0..nullity) |col| {
+            const limit = equations[height][col + rank];
+            const vs = blk: {
+                var vs: [MAX_JOLTAGES]i32 = undefined;
+                for (0..MAX_JOLTAGES) |index| {
+                    vs[index] = equations[index][rank + col];
+                }
+                break :blk vs;
+            };
+            const cost = blk: {
+                var sum: i32 = 0;
+                for (0..rank) |index| sum += vs[index];
+                break :blk lcm - sum;
+            };
+            bases.push(.{
+                .limit = limit,
+                .cost = cost,
+                .vs = vs,
+            });
+        }
+
+        return .{
+            .rank = rank,
+            .nullity = nullity,
+            .lcm = lcm,
+            .rhs = rhs,
+            .bases = bases,
+        };
+    }
+
+    pub fn solution(self: Self) i32 {
+        var sum: i32 = 0;
+        for (self.rhs[0..self.rank]) |value| sum += value;
+        return sum;
+    }
+};
+
+fn Biterator(comptime T: type) type {
+    const info = @typeInfo(T);
+    if (info != .int and info != .float)
+        @compileError("Biterator only works on numeric types");
+
+    return struct {
+        t: T,
+        _t: T,
+
+        pub fn next(self: *@This()) ?usize {
+            const one: T = @intCast(1);
+            if (self.t == 0) {
+                return null;
+            } else {
+                const tz = @ctz(self.t);
+                self.t = self.t ^ (one << @intCast(tz));
+                return tz;
+            }
+        }
+
+        pub fn reset(self: *@This()) void {
+            self.t = self._t;
+        }
+    };
+}
+pub fn biterator(comptime T: type, n: T) Biterator(T) {
+    return .{
+        .t = n,
+        ._t = n,
+    };
+}
+
+inline fn least_common_multiple(a_in: i32, b_in: i32) i32 {
+    const gcd = blk: {
+        var a = a_in;
+        var b = b_in;
+
+        while (b != 0) {
+            const temp = @rem(a, b);
+            a = b;
+            b = temp;
+        }
+
+        break :blk a;
+    };
+
+    return a_in * @divTrunc(b_in, gcd);
+}
+
+fn find_swap_index(rank: usize, height: usize, equations: [MAX_JOLTAGES][MAX_BUTTONS]i32) ?usize {
+    var index: ?usize = null;
+    var min_abs: ?u32 = null;
+    var row = rank;
+
+    while (row < height) : (row += 1) {
+        const v = equations[row][rank];
+        if (v != 0) {
+            const is_new_min = if (min_abs) |min| @abs(v) < min else true;
+            if (is_new_min) {
+                index = row;
+                min_abs = @abs(v);
+            }
+        }
+    }
+
+    return index;
+}
+
+const Bounds = struct {
+    min_index: usize,
+    lower: i32,
+    upper: i32,
+    const Self = @This();
+
+    pub fn find(subspace: Subspace, remaining: u32, rhs_in: [MAX_JOLTAGES]i32) ?Self {
+        var rhs = rhs_in;
+        var it = biterator(u32, remaining);
+        while (it.next()) |bit_index| {
+            const free = subspace.bases.items[bit_index];
+            for (0..subspace.rank) |index| {
+                const v = free.vs[index];
+                if (v < 0) {
+                    rhs[index] -= v * free.limit;
+                }
+            }
+        }
+
+        var bounds: ?Self = null;
+        var min_size: i32 = math.maxInt(i32);
+
+        it.reset();
+        while (it.next()) |min_index| {
+            const free = subspace.bases.items[min_index];
+            var lower: i32 = 0;
+            var upper: i32 = free.limit;
+
+            for (0..subspace.rank) |index| {
+                const t = rhs[index];
+                const v = free.vs[index];
+
+                if (v > 0) {
+                    upper = @min(upper, @divTrunc(t, v));
+                } else if (v < 0) {
+                    lower = @max(@divTrunc((t + v * free.limit) + v + 1, v), lower);
+                }
+            }
+
+            const size = upper - lower + 1;
+            if (size > 0 and size < min_size) {
+                min_size = size;
+                bounds = .{
+                    .min_index = min_index,
+                    .lower = lower,
+                    .upper = upper,
+                };
+            }
+        }
+
+        return bounds;
+    }
+};
+
+fn recurse(subspace: Subspace, rhs_in: [MAX_JOLTAGES]i32, remaining_in: u32, presses: i32) ?i32 {
+    var rhs = rhs_in;
+    const bounds = Bounds.find(subspace, remaining_in, rhs_in) orelse return null;
+    const remaining = remaining_in ^ (@as(u32, 1) << @intCast(bounds.min_index));
+    const basis = subspace.bases.items[bounds.min_index];
+
+    if (remaining != 0) {
+        for (0..subspace.rank) |index| {
+            rhs[index] -= (bounds.lower - 1) * basis.vs[index];
+        }
+
+        var minimum_opt: ?i32 = null;
+        var n: i32 = bounds.lower;
+
+        while (n <= bounds.upper) : (n += 1) {
+            for (0..subspace.rank) |index| {
+                rhs[index] -= basis.vs[index];
+            }
+
+            if (recurse(subspace, rhs, remaining, presses + @as(i32, @intCast(n)) * basis.cost)) |new_minimum| {
+                minimum_opt = @min(minimum_opt orelse new_minimum, new_minimum);
+            }
+        }
+
+        return minimum_opt;
+    }
+
+    if (basis.cost >= 0) {
+        var n: i32 = bounds.lower;
+        while (n <= bounds.upper) : (n += 1) {
+            if (check(@intCast(n), subspace, rhs, basis, presses)) |minimum| {
+                return minimum;
+            }
+        }
+
+        return null;
+    }
+
+    var n = bounds.upper;
+    while (n >= bounds.lower) : (n -= 1) {
+        if (check(n, subspace, rhs, basis, presses)) |minimum| {
+            return minimum;
+        }
+    }
+
+    return null;
+}
+fn check(n: i32, subspace: Subspace, rhs: [MAX_JOLTAGES]i32, basis: Basis, presses: i32) ?i32 {
+    for (0..subspace.rank) |index| {
+        const r = rhs[index];
+        const v = basis.vs[index];
+
+        if (@rem(r - n * v, subspace.lcm) != 0) {
+            return null;
+        }
+    }
+
+    return @divTrunc(presses + n * basis.cost, subspace.lcm);
+}
+
 fn part2(_: Allocator) !usize {
-    return 42;
+    var result: usize = 0;
+    for (MACHINES) |machine| {
+        result += machine.configure_joltages();
+    }
+    return result;
 }
 
 pub const solution: Solution = .{
     .day = .@"10",
     .p1 = .{ .f = part1, .expected = 547 },
-    .p2 = .{ .f = part2, .expected = 42 },
+    .p2 = .{ .f = part2, .expected = 21111 },
 };
 
 test "day10 part1" {
@@ -233,7 +471,7 @@ test "day10 part1" {
 }
 
 test "day10 part2" {
-    _ = try aoc.validate(testing.allocator, part2, 42, .@"10", .two);
+    _ = try aoc.validate(testing.allocator, part2, 21111, .@"10", .two);
 }
 
 test "day10 solution" {
