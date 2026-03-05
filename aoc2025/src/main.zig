902147b379f2b3246a57426609cb0efbc0816a37
roy.crippen4 <roy.crippen4@archarithms.com>
2026-03-05 16:24:12 -0500

    refactor(2025): swap out allocator

--- aoc2025/src/main.zig
+++ aoc2025/src/main.zig
@@ -3,33 +3,25 @@ const builtin = @import("builtin");
 
 const aoc = @import("libaoc");
 
-var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
-
 pub fn main() !void {
-    const gpa, const is_debug = gpa: {
-        break :gpa switch (builtin.mode) {
-            .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
-            .ReleaseFast, .ReleaseSmall => .{ std.heap.smp_allocator, false },
-        };
-    };
-    defer if (is_debug) {
-        _ = debug_allocator.deinit();
-    };
+    var area_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer area_state.deinit();
+    const arena = area_state.allocator();
 
     var total_time: u64 = 0;
 
-    total_time += try @import("day01.zig").solution.solve(gpa);
-    total_time += try @import("day02.zig").solution.solve(gpa);
-    total_time += try @import("day03.zig").solution.solve(gpa);
-    total_time += try @import("day04.zig").solution.solve(gpa);
-    total_time += try @import("day05.zig").solution.solve(gpa);
-    total_time += try @import("day06.zig").solution.solve(gpa);
-    total_time += try @import("day07.zig").solution.solve(gpa);
-    total_time += try @import("day08.zig").solution.solve(gpa);
-    total_time += try @import("day09.zig").solution.solve(gpa);
-    total_time += try @import("day10.zig").solution.solve(gpa);
-    total_time += try @import("day11.zig").solution.solve(gpa);
-    total_time += try @import("day12.zig").solution.solve(gpa);
+    total_time += try @import("day01.zig").solution.solve(arena);
+    total_time += try @import("day02.zig").solution.solve(arena);
+    total_time += try @import("day03.zig").solution.solve(arena);
+    total_time += try @import("day04.zig").solution.solve(arena);
+    total_time += try @import("day05.zig").solution.solve(arena);
+    total_time += try @import("day06.zig").solution.solve(arena);
+    total_time += try @import("day07.zig").solution.solve(arena);
+    total_time += try @import("day08.zig").solution.solve(arena);
+    total_time += try @import("day09.zig").solution.solve(arena);
+    total_time += try @import("day10.zig").solution.solve(arena);
+    total_time += try @import("day11.zig").solution.solve(arena);
+    total_time += try @import("day12.zig").solution.solve(arena);
 
     var buf: [64]u8 = undefined;
     const time = try aoc.time.color(total_time, &buf);
