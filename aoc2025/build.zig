bc96f6ab209a0ec5f751d67bfcc25f20866d682b
roy.crippen4 <roy.crippen4@archarithms.com>
2026-03-05 15:41:36 -0500

    chore(2025): remove `util` module It never got used

--- aoc2025/build.zig
+++ aoc2025/build.zig
@@ -27,12 +27,6 @@ pub fn build(b: *std.Build) void {
     });
     const setup = b.addRunArtifact(setup_exe);
 
-    const util_mod = b.createModule(.{
-        .root_source_file = b.path("src/util/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -41,10 +35,6 @@ pub fn build(b: *std.Build) void {
             .{
                 .name = "libaoc",
                 .module = libaoc_dependency.module("libaoc"),
-            },
-            .{
-                .name = "util",
-                .module = util_mod,
             },
         },
     });
@@ -81,12 +71,8 @@ pub fn build(b: *std.Build) void {
         const setup_unit_tests = b.addTest(.{ .root_module = setup_mod, .filters = test_filters });
         const run_setup_unit_tests = b.addRunArtifact(setup_unit_tests);
 
-        const util_unit_tests = b.addTest(.{ .root_module = util_mod, .filters = test_filters });
-        const run_util_unit_tests = b.addRunArtifact(util_unit_tests);
-
         const test_step = b.step("test", "Run unit tests");
         test_step.dependOn(&run_exe_unit_tests.step);
-        test_step.dependOn(&run_util_unit_tests.step);
         test_step.dependOn(&run_setup_unit_tests.step);
     }
 
@@ -94,10 +80,8 @@ pub fn build(b: *std.Build) void {
     {
         const exe_check = b.addExecutable(.{ .name = "aoc2024", .root_module = exe_mod });
         const setup_check = b.addExecutable(.{ .name = "setup", .root_module = setup_mod });
-        const util_check = b.addExecutable(.{ .name = "util", .root_module = util_mod });
         const check = b.step("check", "Check if it compiles");
         check.dependOn(&exe_check.step);
-        check.dependOn(&util_check.step);
         check.dependOn(&setup_check.step);
     }
 }
--- aoc2025/src/util/root.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub const types = @import("./types.zig");
-pub const array = @import("./array.zig");
-
-test {
-    std.testing.refAllDeclsRecursive(@This());
-}
